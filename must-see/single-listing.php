a63e783773df95fbcd3b6d3a949a5670aa71261b
Chris <cnichols.mail@gmail.com>
2013-12-10 14:01:52 -0600

    Removed duplicate folder

--- must-see/single-listing.php
+++
@@ -1,6 +0,0 @@
-<?php
-remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
-remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-remove_action( 'genesis_after_entry', 'genesis_do_author_box_single' );
-
-genesis();
